78ffc14e4f4a9271e2679e2a7634e99105e43815
karklinsv <Vitalijs.Karklins@4finance.com>
2020-05-19 17:13:20 +0300

    Change js

--- getvisits.js
+++ getvisits.js
@@ -1,11 +1,9 @@
 /*https://stackoverflow.com/questions/32038180/need-example-of-calling-aws-lambda-from-javascript*/
-function callAwsLambdaFunction() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("visits").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "https://ifsmy94ndd.execute-api.eu-west-1.amazonaws.com/visits", true);
-    xhttp.send();
-}
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("visits").innerHTML = this.responseText;
+    }
+};
+xhttp.open("GET", "https://ifsmy94ndd.execute-api.eu-west-1.amazonaws.com/visits", true);
+xhttp.send();
