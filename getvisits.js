b0bf50f6af8d40b05d19c3bdf526870a22c73808
karklinsv <Vitalijs.Karklins@4finance.com>
2020-05-19 16:52:06 +0300

    Change footer

--- getvisits.js
+++ getvisits.js
@@ -3,7 +3,7 @@ function callAwsLambdaFunction() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("visits").innerHTML = "page visited: " + this.responseText;
+            document.getElementById("visits").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", "https://ifsmy94ndd.execute-api.eu-west-1.amazonaws.com/visits", true);
