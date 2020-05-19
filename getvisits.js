1fccb22932d54abc8246ff479190751bd608b3b5
karklinsv <Vitalijs.Karklins@4finance.com>
2020-05-19 16:46:51 +0300

    Add vivists counter

---
+++ getvisits.js
@@ -0,0 +1,11 @@
+/*https://stackoverflow.com/questions/32038180/need-example-of-calling-aws-lambda-from-javascript*/
+function callAwsLambdaFunction() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("visits").innerHTML = "page visited: " + this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://ifsmy94ndd.execute-api.eu-west-1.amazonaws.com/visits", true);
+    xhttp.send();
+}
